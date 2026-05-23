bafe808a0e925a0713f16ef8b9dd743ac9aae894
yummyukos <yummyukos@gmail.com>
2026-05-23 02:49:04 +0000

    remove: Twitter/Xセクション削除（X側embed widget不安定のため）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import InvitationRewards from "@/components/InvitationRewards";
 import EventInfo from "@/components/EventInfo";
 import AchievementProgress from "@/components/AchievementProgress";
 import TicketCta from "@/components/TicketCta";
-import TwitterFeed from "@/components/TwitterFeed";
 import Footer from "@/components/Footer";
 import FloatingTicketCta from "@/components/FloatingTicketCta";
 
@@ -21,7 +20,6 @@ export default function Page() {
       <EventInfo />
       <AchievementProgress />
       <TicketCta />
-      <TwitterFeed />
       <Footer />
 
       {/* 右下に常時表示されるチケット購入ボタン */}
